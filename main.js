5ca6b103a07b94d39fca2e2a27e860dfb2514a38
ssoonD <ksj96226@gmail.com>
2020-09-12 09:38:05 +0900

    Update moving eyes

---
+++ main.js
@@ -0,0 +1,26 @@
+const eyes = function (selector) {
+  const eye = document.querySelector(selector),
+    puple = eye.querySelector(".pupil"),
+    eyeArea = eye.getBoundingClientRect();
+
+  const moveEye = function (mouseX, mouseY) {
+    const radian = Math.atan2(
+      mouseY - (eyeArea.y + eyeArea.height * 0.5),
+      mouseX - (eyeArea.x + eyeArea.width * 0.5)
+    );
+    puple.style.transform =
+      "rotate(" + ((180 * radian) / Math.PI - 90) + "deg)";
+  };
+
+  return {
+    moveEye: moveEye,
+  };
+};
+
+const leftEye = eyes(".eye-1");
+const rightEye = eyes(".eye-2");
+
+window.addEventListener("mousemove", (e) => {
+  leftEye.moveEye(e.pageX, e.pageY);
+  rightEye.moveEye(e.pageX, e.pageY);
+});
